3b77c9c0f0566c9c97e5f64fc292c5fb51540b3d
a.norouzian <aliinorouzian@gmail.com>
2024-10-20 18:21:32 +0330

    feat: repo and uow added

---
+++ src/Core/BhvrIV.Domain/Contracts/Persistence/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using BhvrIV.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace BhvrIV.Domain.Contracts.Persistence;
+
+public interface IRepository<TEntity> where TEntity : EntityBase
+{
+    Task Add(TEntity entity);
+    Task Add(List<TEntity> entity);
+    void Update(TEntity entity);
+    void Update(List<TEntity> entity);
+
+    Task<int> Update(Expression<Func<TEntity, bool>> where,
+        Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> update);
+
+    void Remove(TEntity entity);
+    void Remove(List<TEntity> entity);
+    Task<int> Remove(Expression<Func<TEntity, bool>>? where = null);
+
+    Task<TEntity?> First(Expression<Func<TEntity, bool>>? where = null,
+        List<Expression<Func<TEntity, object>>>? includes = null);
+
+    Task<List<TEntity>> List(Expression<Func<TEntity, bool>>? where = null,
+        List<Expression<Func<TEntity, object>>>? includes = null);
+
+    Task<bool> Any(Expression<Func<TEntity, bool>>? where = null,
+        List<Expression<Func<TEntity, object>>>? includes = null);
+
+    Task<int> Count(Expression<Func<TEntity, bool>>? where = null,
+        List<Expression<Func<TEntity, object>>>? includes = null);
+}
---
+++ src/Core/BhvrIV.Domain/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using BhvrIV.Domain.Entities.Common;
+
+namespace BhvrIV.Domain.Contracts.Persistence;
+public interface IUnitOfWork
+{
+    public IRepository<TEntity> GetRepository<TEntity>(bool onlyRead = false) where TEntity : EntityBase;
+    Task<int> SaveChanges();
+}
